4934bddaa4b8b22c7fe3225bc29ba01c14440526
nishant <nishantshankar226@gmail.com>
2020-11-10 19:23:40 -0500

    Add POST request

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import Flask
+from flask import render_template, flash, redirect, request, url_for, jsonify
+
 from app import app
 
 @app.route('/')
@@ -6,3 +8,9 @@ from app import app
 def index():
     user = {'username': 'Rishabh'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    print(request.json['genre'])
+    print(request.json['text'])
+    return jsonify({'success':True}), 200, {'ContentType':'application/json'} 
