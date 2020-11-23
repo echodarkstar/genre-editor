aab329661692a1b84ca125112b62b68a16ade4f8
RishabhKhot <51913409+RishabhKhot@users.noreply.github.com>
2020-11-23 12:12:05 -0500

    Updated rotes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, flash, redirect, request, url_for, jsonify
+from nltk.corpus import wordnet
 
 from app import app
 
@@ -14,3 +15,16 @@ def generate():
     print(request.json['genre'])
     print(request.json['text'])
     return jsonify({'success':True}), 200, {'ContentType':'application/json'} 
+
+@app.route('/synonyms', methods=['POST'])
+def synonyms():
+    synonyms = []
+    if request.json['selectedText']:
+        for syn in wordnet.synsets(request.json['selectedText']): 
+            for l in syn.lemmas(): 
+                synonyms.append(l.name()) 
+        return jsonify({'success':True, 'synonyms':synonyms[0], 'synonyms1':synonyms[1]}), 200, {'ContentType':'application/json'}
+    print(set(synonyms)) 
+    print(request.json['selectedText'])
+    return jsonify({'success':True, 'synonyms':"", 'synonyms1':""}), 200, {'ContentType':'application/json'} 
+
