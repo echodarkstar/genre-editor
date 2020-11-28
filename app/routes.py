505a757383c3456dc02ed2d4e525e6d3eedacac1
RishabhKhot <51913409+RishabhKhot@users.noreply.github.com>
2020-11-28 13:43:41 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask import render_template, flash, redirect, request, url_for, jsonify
 from nltk.corpus import wordnet
 
-from app import app
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Rishabh'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'MindBender'}
+    return render_template('test.html', title='Home', user=user)
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -22,9 +22,9 @@ def synonyms():
     if request.json['selectedText']:
         for syn in wordnet.synsets(request.json['selectedText']): 
             for l in syn.lemmas(): 
-                synonyms.append(l.name()) 
+                if (l.name().lower() != request.json['selectedText'].lower()) and l.name() not in synonyms:
+                    synonyms.append(l.name()) 
         return jsonify({'success':True, 'synonyms':synonyms[0], 'synonyms1':synonyms[1]}), 200, {'ContentType':'application/json'}
-    print(set(synonyms)) 
     print(request.json['selectedText'])
     return jsonify({'success':True, 'synonyms':"", 'synonyms1':""}), 200, {'ContentType':'application/json'} 
 
